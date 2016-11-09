7eec51763243faefd420c18751500a20ef006ac4
Michael Wunderlich <mwunderl@example.com>
2016-11-09 08:07:12 +0000

    configuration files

---
+++ wp-config.php
@@ -0,0 +1,20 @@
+<?php
+define('DB_NAME', $_SERVER['RDS_DB_NAME']);
+define('DB_USER', $_SERVER['RDS_USERNAME']);
+define('DB_PASSWORD', $_SERVER['RDS_PASSWORD']);
+define('DB_HOST', $_SERVER['RDS_HOSTNAME']);
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+define('AUTH_KEY',         $_SERVER['AUTH_KEY']);
+define('SECURE_AUTH_KEY',  $_SERVER['SECURE_AUTH_KEY']);
+define('LOGGED_IN_KEY',    $_SERVER['LOGGED_IN_KEY']);
+define('NONCE_KEY',        $_SERVER['NONCE_KEY']);
+define('AUTH_SALT',        $_SERVER['AUTH_SALT']);
+define('SECURE_AUTH_SALT', $_SERVER['SECURE_AUTH_SALT']);
+define('LOGGED_IN_SALT',   $_SERVER['LOGGED_IN_SALT']);
+define('NONCE_SALT',       $_SERVER['NONCE_SALT']);
+$table_prefix  = 'wp_';
+define('WP_DEBUG', false);
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+require_once(ABSPATH . 'wp-settings.php');
