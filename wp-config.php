56c4ce2a6d343f742166d3765887b00cbfedf749
James Ribe <jribe@tableau.com>
2017-08-18 14:21:34 -0700

    Updated wp-config.php to use RDS_PORT

--- wp-config.php
+++ wp-config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_NAME', $_SERVER['RDS_DB_NAME']);
-define('DB_USER', $_SERVER['RDS_USERNAME']);
+define('DB_USER', $_SERVER['RDS_HOSTNAME'] . ':' . $_SERVER['RDS_PORT']);
 define('DB_PASSWORD', $_SERVER['RDS_PASSWORD']);
 define('DB_HOST', $_SERVER['RDS_HOSTNAME']);
 define('DB_CHARSET', 'utf8');
